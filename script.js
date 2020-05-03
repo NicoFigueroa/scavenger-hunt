ba14054dab22ea20402b803c61fdcb1b38a5819e
Nico Figueroa <nifigueroa@wccnet.edu>
2020-05-03 17:22:16 -0400

    First clue, countdown

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var startColor = [218,245,244]
 var endColor = [255,79,0]
 
 var currentClue = null
-
+var scavengerTime = new Date(2020, 4, 3, 17, 20)
 
 var calculateCurrentColor = function() {
 	var clues = document.getElementsByTagName("div").length
@@ -46,7 +46,62 @@ var showRelevantClue = function() {
 	}
 }
 
+var timeUntilScavenger = function() {
+	var secUntil = (scavengerTime.getTime() - Date.now()) / 1000
+	var hours = Math.floor((secUntil / 60 / 60))
+	var min = Math.floor((secUntil / 60 ) % 60)
+	var sec = Math.floor((secUntil % 60))
+
+	if (sec.toString().length < 2) {
+		sec = "0" + sec
+	}
+
+	if (min.toString().length < 2) {
+		min = "0" + min
+	}
+
+	if (hours.toString().length < 2) {
+		hours = "0" + hours
+	}
+
+	return {hours: hours, minutes: min, seconds: sec, totalSeconds: Math.floor(secUntil)}
+}
+
+var updateCountdownTimer = function() {
+
+	if (isScavengerTime()) {
+		document.getElementById("countdown").classList.add("hide")
+	} else {
+		var remaining = timeUntilScavenger()
+
+		var secCell = document.getElementById("seconds")
+		var minuteCell = document.getElementById("minutes")
+		var hourCell = document.getElementById("hours")
+
+		secCell.innerHTML = remaining.seconds
+		minuteCell.innerHTML = remaining.minutes
+		hourCell.innerHTML = remaining.hours
+	}
+}
+
+var isScavengerTime = function() {
+	return (scavengerTime.getTime() - Date.now()) <= 0
+}
+
 window.addEventListener("load", function() {
-	showRelevantClue()
-	initialize()
+
+	if (!isScavengerTime()) {
+		updateCountdownTimer()
+		document.getElementsByTagName("body")[0].classList.add("count")
+		document.getElementById("countdown").classList.toggle("hide")
+
+		setInterval(function() {
+			updateCountdownTimer()
+		}, 1000)
+	} else {
+		showRelevantClue()
+		initialize()
+	}
+
+
 })
