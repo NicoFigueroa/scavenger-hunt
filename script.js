c4e3489629858eed38a549ddc36e7c0c1714a8a2
Nico Figueroa <nifigueroa@wccnet.edu>
2020-05-03 22:48:10 -0400

    enabled timer

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var startColor = [218,245,244]
 var endColor = [255,79,0]
 
 var currentClue = null
-var scavengerTime = new Date(Date.now() - 5000)
-//var scavengerTime = new Date(2020, 4, 5, 17)
+//var scavengerTime = new Date(Date.now() - 5000)
+var scavengerTime = new Date(2020, 4, 5, 17)
 
 var calculateCurrentColor = function() {
 	var clues = document.getElementsByTagName("div").length
